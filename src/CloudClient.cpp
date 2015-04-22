6fe2e9ed91665970159abbd1c015d9a2a71d3e14
lony <lony@smart.net>
2015-04-22 11:58:17 +0800

    Cloud API

---
+++ src/CloudClient.cpp
@@ -0,0 +1,92 @@
+#include "CloudClient.h"
+
+CloudClient::CloudClient(string productId, string productKey, string productSecret)
+    : _productId(productId), _productKey(productKey), _productSecret(productSecret)
+{
+
+}
+
+CloudClient::~CloudClient()
+{
+
+}
+
+void CloudClient::getDeviceToken()
+{
+
+}
+
+void CloudClient::getDateTime()
+{
+
+}
+
+void CloudClient::getVersion()
+{
+
+}
+
+void CloudClient::getUserInfo()
+{
+
+}
+
+void CloudClient::getQRCode()
+{
+
+}
+
+void CloudClient::getMQConfig()
+{
+
+}
+
+void CloudClient::getLocation()
+{
+
+}
+
+void CloudClient::getDeviceProperty()
+{
+
+}
+
+void CloudClient::putDeviceProperty()
+{
+
+}
+
+void CloudClient::registerDevice()
+{
+
+}
+
+void CloudClient::uploadHeartbeat()
+{
+
+}
+
+void CloudClient::uploadLog()
+{
+
+}
+
+void CloudClient::downloadFile()
+{
+
+}
+
+void CloudClient::factoryReset()
+{
+
+}
+
+void CloudClient::createSubDevice()
+{
+
+}
+
+void CloudClient::deleteSubDevice()
+{
+
+}
---
+++ src/CloudClient.h
@@ -0,0 +1,36 @@
+#ifndef CLOUD_CLIENT_H
+#define CLOUD_CLIENT_H
+#include <string>
+#include <http/restclient.h>
+using namespace std;
+
+class CloudClient
+{
+public:
+    CloudClient(string productId, string productKey, string productSecret);
+    ~CloudClient();
+    void getDeviceToken();
+    void getDateTime();
+    void getVersion();
+    void getUserInfo();
+    void getQRCode();
+    void getMQConfig();
+    void getLocation();
+    void getDeviceProperty();
+    void putDeviceProperty();
+    void registerDevice();
+    void uploadHeartbeat();
+    void uploadLog();
+    void downloadFile();
+    void factoryReset();
+    void createSubDevice();
+    void deleteSubDevice();
+
+private:
+    string _productId;
+    string _productKey;
+    string _productSecret;
+
+};
+
+#endif
