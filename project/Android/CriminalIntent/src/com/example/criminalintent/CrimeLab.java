1e386edd897abe95667f77d60eff5e2f5416fd3b
Lony <qyl201307@163.com>
2015-11-09 15:29:26 +0800

    Create ListFragment to show Crimes List.

---
+++ project/Android/CriminalIntent/src/com/example/criminalintent/CrimeLab.java
@@ -0,0 +1,52 @@
+package com.example.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.content.Context;
+
+/***
+ * 
+ * @author Lony
+ * 
+ * Singleton for ArrayList<Crime>, hold data when the App running.
+ */
+
+public class CrimeLab {
+	
+	private static CrimeLab sCrimeLab;
+	
+	private Context mAppContext;
+	private ArrayList<Crime> mCrimes; 
+	
+	private CrimeLab(Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+		for (int i = 0; i < 100; i++) {
+			Crime crime = new Crime();
+			crime.setTitle("Crime #" + i);
+			crime.setSolved(i % 2 == 0);
+			mCrimes.add(crime);
+		}
+	}
+	
+	public static CrimeLab getInstance(Context context) {
+		if (sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(context.getApplicationContext()); // App Singleton use AppContext
+		}
+		return sCrimeLab;
+	}
+
+	public ArrayList<Crime> getCrimes() {
+		return mCrimes;
+	}
+	
+	public Crime getCrime(UUID id) {
+		for (Crime crime : mCrimes) {
+			if (crime.getId() == id) {
+				return crime;
+			}
+		}
+		return null;
+	}
+}
